246540158e654fa580d2763e52f5e0fb20d5cf7e
visakh.cs@iinerds.com <visakh.cs@iinerds.com>
2020-02-28 19:30:28 +0530

    Updates

---
+++ precache-manifest.810f9dca3507bf5ec83ee467aa631388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c03420ee359020738c572df924c2221",
+    "url": "/diagnal-frontendapp/index.html"
+  },
+  {
+    "revision": "7e05ba416caa6f4cbaaf",
+    "url": "/diagnal-frontendapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "49e8d5dc26b6d42c7cf0",
+    "url": "/diagnal-frontendapp/static/js/2.aa75ae7a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/diagnal-frontendapp/static/js/2.aa75ae7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e05ba416caa6f4cbaaf",
+    "url": "/diagnal-frontendapp/static/js/main.38788fe8.chunk.js"
+  },
+  {
+    "revision": "963d09c2cc898927e788",
+    "url": "/diagnal-frontendapp/static/js/runtime-main.16bb483a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/diagnal-frontendapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
